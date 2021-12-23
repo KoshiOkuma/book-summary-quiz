77bb4b1f15154b4ec037ffcbb9243ab2623ac679
KoshiOkuma <koshi.glorydays1128@gmail.com>
2021-12-23 10:00:43 +0900

    UserModel, BookModel修正

--- app/Models/book.php
+++ app/Models/book.php
@@ -5,13 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class book extends Model
+class Book extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'title',
+        'summary',
+        'author',
     ];
+
+
 }
