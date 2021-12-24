4cdedc4a8b915f55d43860638220b522a172e691
KoshiOkuma <koshi.glorydays1128@gmail.com>
2021-12-24 13:36:29 +0900

    認証ミドルウェア

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $books = Book::all();
@@ -35,8 +41,6 @@ class BookController extends Controller
     {
         $book = Book::findOrFail($id);
         $summaries = Book::find($id)->summary;
-
-        // dd($summary);
 
         return view('books.show', compact(['book', 'summaries']));
     }
