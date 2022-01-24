fac876a55800396ab08d15afffb53adb04742abd
KoshiOkuma <koshi.glorydays1128@gmail.com>
2022-01-24 22:01:44 +0900

    Mypageに非表示リスト追加、restore, forceDestory機能実装

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Image;
+use App\Models\Book;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use Image;
 
 class MypageController extends Controller
 {
     public function index()
     {
         $user = User::findOrFail(Auth::id());
+        $notShowing = Book::onlyTrashed()
+        ->where('user_id', Auth::id())->get();
 
-        return view('mypage.index', compact('user'));
+        return view('mypage.index', compact(['user', 'notShowing']));
     }
 
     public function store(Request $request)
@@ -61,6 +64,31 @@ class MypageController extends Controller
         return redirect()->route('mypage.index')
         ->with([
             'message' => "プロフィールを更新しました",
+            'status' => 'info'
+        ]);
+    }
+
+    public function restore($id)
+    {
+        Book::onlyTrashed()
+        ->where('id',$id)->restore();
+
+        return redirect()->route('mypage.index')
+        ->with([
+            'message' => "本を再表示しました",
+            'status' => 'info'
+        ]);
+
+    }
+
+    public function forceDestroy($id)
+    {
+        Book::onlyTrashed()
+        ->where('id',$id)->forceDelete();
+
+        return redirect()->route('mypage.index')
+        ->with([
+            'message' => "本を完全に削除しました",
             'status' => 'info'
         ]);
     }
