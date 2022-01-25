321edb9bac0422ec4cb91bcb52490dd04f8557ce
KoshiOkuma <koshi.glorydays1128@gmail.com>
2022-01-26 06:30:26 +0900

    User側のControllerに認証コンストラクトを追加

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Storage;
 
 class MypageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $user = User::findOrFail(Auth::id());
