541a59df0d917d944f0438684c33ddb54582d76a
KoshiOkuma <koshi.glorydays1128@gmail.com>
2022-02-07 00:24:29 +0900

    login画面の不具合解消

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,9 +61,9 @@
 
             </div>
             <div class="flex justify-between">
-                <x-button>
-                    <a href="{{route('guestLogin')}}">ゲストログイン</a>
-                </x-button>
+                <button>
+                    <a href="{{route('guestLogin')}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">ゲストログイン</a>
+                </button>
                 <x-button class="ml-3 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700">
                     {{ __('Log in') }}
                 </x-button>
